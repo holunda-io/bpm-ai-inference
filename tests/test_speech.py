71cb0af7254e4b3fe4a5820b7ddb0de9ca3b1211
Benjoyo <bennet.krause@gmx.de>
2024-04-02 17:15:32 +0200

    fix test

--- tests/test_speech.py
+++ tests/test_speech.py
@@ -3,11 +3,11 @@ from bpm_ai_inference.speech_recognition.faster_whisper import FasterWhisperASR
 
 async def test_faster_whisper():
     fw = FasterWhisperASR()
-    text = await fw.transcribe("example.mp3")
-    assert text.lower().strip() == "looking with a half-fantastic curiosity to see whether the tender grass of early spring"
+    result = await fw.transcribe("example.mp3")
+    assert result.text.lower().strip() == "looking with a half-fantastic curiosity to see whether the tender grass of early spring"
 
 
 async def test_faster_whisper_url():
     fw = FasterWhisperASR()
-    text = await fw.transcribe("https://upload.wikimedia.org/wikipedia/commons/d/dd/Armstrong_Small_Step.ogg")
-    assert "giant leap for mankind" in text.lower()
+    result = await fw.transcribe("https://upload.wikimedia.org/wikipedia/commons/d/dd/Armstrong_Small_Step.ogg")
+    assert "giant leap for mankind" in result.text.lower()
